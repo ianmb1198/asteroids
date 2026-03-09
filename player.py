6a4492d454174bc95ddbbe6ba0fe94a75a18430b
ianmb1198 <ianmb1198@comcast.net>
2026-03-09 18:10:32 -0500

    added move method to Player class

--- player.py
+++ player.py
@@ -30,3 +30,10 @@ class Player(CircleShape):
 
 		if keys[pygame.K_d]:
 			self.rotate(dt)
+
+
+	def move(self, dt):
+		unit_vector = pygame.Vector(0, 1)
+		rotated_vector = unit_vector.rotate(self.rotation)
+		rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+		self.position += rotated_with_speed_vector
