3ddea9df72d07b83de1178d5feec37c30c27d879
ianmb1198 <ianmb1198@comcast.net>
2026-03-09 18:18:43 -0500

    now calling the move method in the update method

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import *
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -15,12 +15,21 @@ class Player(CircleShape):
 		c = self.position - forward * self.radius + right
 		return [a, b, c]
 
+
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
 
 	def rotate(self, dt):
 		self.rotation += PLAYER_TURN_SPEED * dt
+
+
+	def move(self, dt):
+		unit_vector = pygame.Vector2(0, 1)
+		rotated_vector = unit_vector.rotate(self.rotation)
+		rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+		self.position += rotated_with_speed_vector
+
 
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
@@ -31,9 +40,10 @@ class Player(CircleShape):
 		if keys[pygame.K_d]:
 			self.rotate(dt)
 
+		if keys[pygame.K_w]:
+			self.move(dt)
 
-	def move(self, dt):
-		unit_vector = pygame.Vector(0, 1)
-		rotated_vector = unit_vector.rotate(self.rotation)
-		rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-		self.position += rotated_with_speed_vector
+		if keys[pygame.K_s]:
+			self.move(-dt)
+
+
