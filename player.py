98078a22da32496bc33ff022f286888b390965e3
ianmb1198 <ianmb1198@comcast.net>
2026-03-11 14:38:16 -0500

    Update: added shot cooldown timer variable to Player constructor

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.shot_cooldown_timer = 0
 
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
